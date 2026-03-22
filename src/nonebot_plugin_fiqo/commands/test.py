7a42f02b724df61f5a13483d82091e448159c97a
Christoph Chen <ugamegerm.chen@gmail.com>
2026-03-21 23:34:12 -0400

    fix(fiqo_test): remove unnecessary message sent

--- src/nonebot_plugin_fiqo/commands/test.py
+++ src/nonebot_plugin_fiqo/commands/test.py
@@ -1,7 +1,6 @@
 from nonebot import logger
 from nonebot_plugin_alconna import (
     Alconna,
-    UniMessage,
     CommandMeta,
     on_alconna,
 )
@@ -28,6 +27,4 @@ fiqo_test = on_alconna(
 @fiqo_test.handle()
 async def _():
     logger.info("Received test command")
-    response = UniMessage("This is a test message.")
-    await fiqo_test.send(response)
     raise RuntimeError("This is a test error to check error handling.")
